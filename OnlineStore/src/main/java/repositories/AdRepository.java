29922bc69ee5e47dffe77fe9627ba761b3d171d6
rezamars <reza.mars@yahoo.com>
2017-09-10 12:07:48 +0200

    Endpoint (ads) now working.

--- OnlineStore/src/main/java/repositories/AdRepository.java
+++ OnlineStore/src/main/java/repositories/AdRepository.java
@@ -2,15 +2,19 @@ package repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import entities.Ad;
 
-@RepositoryRestResource(collectionResourceRel = "ads", path = "ads")
-public interface AdRepository extends PagingAndSortingRepository<Ad,Long>{
+//@RepositoryRestResource(path = "ads")
+public interface AdRepository extends CrudRepository<Ad,Long>{
 	
-	List<Ad> findByHeadLine(@Param("headline") String headline);
+	//List<Ad> findByHeadLine(@Param("headline") String headline);
 
 }
--- OnlineStore/src/main/java/springApp/Application.java
+++ OnlineStore/src/main/java/springApp/Application.java
@@ -3,14 +3,22 @@ package springApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import entities.Ad;
+import repositories.AdRepository;
+
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {AdRepository.class})
+@EntityScan(basePackageClasses = {Ad.class})
+@EnableJpaRepositories(basePackageClasses = {AdRepository.class})
 @PropertySource("application.properties")
 public class Application {
 
